838b0e5399c7a2aee45aa844f22e7416c6d67a02
cittatva <cittatva@gmail.com>
2014-08-15 11:28:45 -0500

    separated the running loop from the Positions class file, cleaned up tests

--- positions.rb
+++ positions.rb
@@ -72,20 +72,3 @@ class Position
 	end
 end
 
-positions = Array.new
-positions << Position.new("mnkd", 482, 8.19)
-positions << Position.new("mnga", 1000, 1.29)
-positions << Position.new("tsla", 8, 249.91)
-
-loop do
-	total_profit = 0
-	positions.each do |position|
-		position.update_if_stale
-		puts "#{position.symbol.upcase}, $#{position.avg_price}, $#{position.current_price}, #{position.size}, $#{position.invested}, $#{position.value}, $#{position.profit}"
-		total_profit += position.profit
-	end
-	puts total_profit.round(2)
-	STDOUT.flush
-	sleep(60)
-end
-
